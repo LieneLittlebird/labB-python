339aa2042b7787edebe695ca6394686dea353c42
LieneLittlebird <liene.putnina20@gmail.com>
2021-03-26 20:00:25 +0200

    Added the validation function and defined the variable n

--- labB.py
+++ labB.py
@@ -6,19 +6,24 @@
 # Create and use a function for this check.
 # Program created at: 2021/26.03
 
-#1. Define n and sort out its usage
-#2. Write validation function (negative also), add it to every input
-
-
+# A recursive function finds the nth Fibonacci term.
+# It checks the entered number and returns the sum of the two previous numbers in
+# the Fibonacci sequence, if the number is not smaller or equal to 1 (0 and 1 ar the first two Fibonacci numbers)
 def findFibonacci(n):
-    # n = int
     if n <= 1:
         return n
     else:
         return findFibonacci(n - 1) + findFibonacci(n - 2)
 
+# A function checks, if the entered number is a Fibonacci number.
+# It starts with 0 and 1, since those are the first two Fibonacci numbers.
+# They are added to each other to get their Fibonacci number.
+# Then a while loop is executed, which stops only when the variable "next" is not smaller than the input number.
+# In each iteration the variables are assigned a new value thus incrementing the initial
+# number and getting to the next term in the Fibonacci sequence.
+# If next (each Fibonacci number) is equal to the input number, the input number IS a Fibonacci number.
+# If not, it is NOT a Fibonacci number.
 def isFibonacci(n):
-    # n = int
     if n <= 1:
         print(f"{n} is a Fibonacci number.\n")
     else:
@@ -34,46 +39,56 @@ def isFibonacci(n):
         if next == n:
             print(f"{n} is a Fibonacci number.\n")
         else:
-            print(f"{n} is NOT  a Fibonacci number.\n")
+            print(f"{n} is NOT a Fibonacci number.\n")
 
-def getInput: 
-    inputNumber = 0 #Does it work like that?
+# Input validation function to keep the execution loop cleaner
+# This function will be called each time an input is needed. 
+def getInput(): 
+    inputNumber = None 
     continueEntering = True
-        while continueEntering:
-            try:
-                inputNumber = int(input()) #Does it work like that?
-                assert inputNumber > 0
-            except ValueError:
-                print("Invalid input. Please, enter an integer.")
-                continue
-            except AssertionError:
-                print("Invalid input. Please, enter a positive integer.")
-            else:
-                continueEntering = False
-
-def exerciseOne:
-    findFibNum = int(input("Enter a number to get the nth Fibonacci term: "))
-    fibonacciNumber = findFibonacci(n)
-    print(f"The Fibonacci term Nr. {n} is {fibonacciNumber}")
+    while continueEntering:
+        try:
+            inputNumber = int(input()) 
+            assert inputNumber >= 0
+        except ValueError:
+            print("Invalid input. Please, enter an integer.")
+            continue
+        except AssertionError:
+            print("Invalid input. Please, enter a positive integer.")
+        else:
+            continueEntering = False
     
-def exerciseTwo:
-    checkFibNum = int(
-        int(input("Enter a number to see if it is a part of the Fibonacci sequence: "))
-    )
-    print(isFibonacci(n))
+    return inputNumber
 
+# A function for exercise one (finding the Fibonacci numbers).
+# This function deals with the input/output of the exercise and
+# executes the function to find the Fibonacci term for the input number.
+def exerciseOne():
+    print("Enter a number to get the nth Fibonacci term:")
+    n = getInput()
+    fibonacciNumber = findFibonacci(n)
+    print(f"The Fibonacci term Nr.{n} is {fibonacciNumber}\n")
+
+# A function for exercise two (checking if the input is a Fibonacci number).
+# This function deals with the input/output of the exercise and
+# executes the function to check if the input is a Fibonacci number.
+def exerciseTwo(): 
+    print("Enter a number to see if it is a part of the Fibonacci sequence: ")
+    n = getInput()
+    isFibonacci(n)
+
+# Execution of the program
 repeatExec = True
 while repeatExec:
-    print(exerciseOne())
-    print(exerciseTwo())
+    exerciseOne()
+    exerciseTwo()
 
-    count(int(input("Enter 1 to continue or 0 to quit \n"))) #Add validation somehow
-    # print("Enter 1 to continue or 0 to quit \n")
+    print("Enter 1 to continue or 0 to quit")
+    continueExec = getInput()
 
-    if count == 0:
+    if continueExec == 0:
         repeatExec = False
-        # repeatExec = getInput()
-
         break
+
     else:
         repeatExec = True
